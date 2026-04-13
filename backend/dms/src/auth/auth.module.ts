f082e3a1da1ccf33a459fccea640bbea24ee1a0f
utujesandrine456 <utujesandrine456@gmail.com>
2026-04-13 18:50:09 +0200

    Latest Deployment

--- backend/dms/src/auth/auth.module.ts
+++ backend/dms/src/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'CyberSecurity',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1d'},
     }),
   ],
